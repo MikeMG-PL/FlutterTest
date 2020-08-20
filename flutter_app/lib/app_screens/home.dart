fb13cc6cc452514ee347714d6059dcd158fdcee8
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2020-08-20 04:43:13 +0200

    trying to put image into the app

--- flutter_app/lib/app_screens/home.dart
+++ flutter_app/lib/app_screens/home.dart
@@ -5,39 +5,116 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+          padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 40.0),
           alignment: Alignment.center,
           color: Colors.greenAccent,
-          child: Row(
+          child: Column(
             children: [
-              Expanded(
-                  child: Center(
-                      child: Text("Flight",
-                          textDirection: TextDirection.ltr,
-                          style: TextStyle(
-                              decoration: TextDecoration.none,
-                              fontSize: 20.0,
-                              fontFamily: "Alata",
-                              color: Colors.pinkAccent)))),
-              Expanded(
-                  child: Center(
-                      child: Text("Flight",
-                          textDirection: TextDirection.ltr,
-                          style: TextStyle(
-                              decoration: TextDecoration.none,
-                              fontSize: 20.0,
-                              fontFamily: "Alata",
-                              color: Colors.pinkAccent)))),
-              Expanded(
-                  child: Center(
-                      child: Text("Flight",
-                          textDirection: TextDirection.ltr,
-                          style: TextStyle(
-                              decoration: TextDecoration.none,
-                              fontSize: 20.0,
-                              fontFamily: "Alata",
-                              color: Colors.pinkAccent))))
+              Row(
+                children: [
+                  Expanded(
+                      child: Center(
+                          child: Text("Flight",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  fontSize: 20.0,
+                                  fontFamily: "Alata",
+                                  color: Colors.pinkAccent)))),
+                  Expanded(
+                      child: Center(
+                          child: Text("Flight",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  fontSize: 20.0,
+                                  fontFamily: "Alata",
+                                  color: Colors.pinkAccent)))),
+                  Expanded(
+                      child: Center(
+                          child: Text("Flight",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  fontSize: 20.0,
+                                  fontFamily: "Alata",
+                                  color: Colors.pinkAccent))))
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Center(
+                          child: Text("Flight",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  fontSize: 20.0,
+                                  fontFamily: "Alata",
+                                  color: Colors.pinkAccent)))),
+                  Expanded(
+                      child: Center(
+                          child: Text("Flight",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  fontSize: 20.0,
+                                  fontFamily: "Alata",
+                                  color: Colors.pinkAccent)))),
+                  Expanded(
+                      child: Center(
+                          child: Text("Flight",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  fontSize: 20.0,
+                                  fontFamily: "Alata",
+                                  color: Colors.pinkAccent))))
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Center(
+                          child: Text("Flight",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  fontSize: 20.0,
+                                  fontFamily: "Alata",
+                                  color: Colors.pinkAccent)))),
+                  Expanded(
+                      child: Center(
+                          child: Text("Flight",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  fontSize: 20.0,
+                                  fontFamily: "Alata",
+                                  color: Colors.pinkAccent)))),
+                  Expanded(
+                      child: Center(
+                          child: Text("Flight",
+                              textDirection: TextDirection.ltr,
+                              style: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  fontSize: 20.0,
+                                  fontFamily: "Alata",
+                                  color: Colors.pinkAccent))))
+                ],
+              ),
+              Clovermike()
             ],
           )),
     );
+  }
+}
+
+class Clovermike extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage("images/clovermike.png");
+    Image image = Image(image: assetImage);
+    return Container(child: image);
   }
 }
