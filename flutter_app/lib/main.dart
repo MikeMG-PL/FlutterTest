d2183b00026d55882966df5b3a912722ec7aaf2e
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2020-08-20 12:36:27 +0200

    ending section 2

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,57 @@
 import "package:flutter/material.dart";
-import './app_screens/home.dart';
+//import './app_screens/home.dart';
 
 void main() {
-  runApp(MaterialApp(title: "Exploring UI Widgets", home: Home()));
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Exploring UI Widgets",
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          backgroundColor: Colors.grey,
+          tooltip: "Add one more item",
+          onPressed: () {},
+        ),
+        body: getListView(),
+        appBar: AppBar(
+            title: Text("Long list", style: TextStyle(fontFamily: "Alata"))),
+      )));
+}
+
+void showSnackBar(BuildContext context) {
+  var snackBar = SnackBar(
+      content: Text("Item added"),
+      action: SnackBarAction(
+          label: "UNDO",
+          onPressed: () {
+            undoneBar(context);
+          }));
+  Scaffold.of(context).showSnackBar(snackBar);
+}
+
+void undoneBar(BuildContext context) {
+  var snackBar = SnackBar(content: Text("Undone"));
+  Scaffold.of(context).showSnackBar(snackBar);
+}
+
+List<String> getListElements() {
+  var items = List<String>.generate(1000, (counter) => "Item $counter");
+  return items;
+}
+
+Widget getListView() {
+  var listItems = getListElements();
+
+  var listView = ListView.builder(
+      itemCount: listItems.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+            onTap: () {
+              showSnackBar(context);
+            },
+            leading: Icon(Icons.arrow_right),
+            title: Text(listItems[index]));
+      });
+
+  return listView;
 }
